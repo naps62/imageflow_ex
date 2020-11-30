1f520f86331716d9020cafc2a245d0de88dc4da8
Miguel Palhas <mpalhas@gmail.com>
2020-11-30 22:45:44 +0000

    Adding ex_doc

--- mix.exs
+++ mix.exs
@@ -17,20 +17,15 @@ defmodule Imageflow.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    [extra_applications: [:logger]]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:rustler, "~> 0.21.1"},
-      {:jason, "~> 1.2"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:jason, "~> 1.2"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
