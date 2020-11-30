7e18c2e828fa5c909aa271738af1418f6436ba8c
Miguel Palhas <mpalhas@gmail.com>
2020-11-30 01:18:06 +0000

    Removing old commented code

--- native/imageflow_ex/src/job.rs
+++ native/imageflow_ex/src/job.rs
@@ -26,7 +26,6 @@ pub fn create<'a>(env: Env<'a>, _args: &[Term<'a>]) -> Result<Term<'a>, Error> {
             let job = Job {
                 inner: Mutex::new(context),
             };
-            // let id = Box::into_raw(context) as usize;
             let re = Box::into_raw(Box::new(job));
 
             Ok((atoms::ok(), re as usize).encode(env))
@@ -36,7 +35,7 @@ pub fn create<'a>(env: Env<'a>, _args: &[Term<'a>]) -> Result<Term<'a>, Error> {
     }
 }
 
-pub fn destroy<'a>(env: Env<'a>, _args: &[Term<'a>]) -> Result<Term<'a>, Error> {
+pub fn destroy<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
     // let id: usize = args[0].decode()?;
     // let ptr = id as *mut Job;
 
@@ -57,20 +56,10 @@ pub fn add_input<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error>
     let mut v = vec![0; 67];
     v.copy_from_slice(bytes.as_slice());
 
-    // let vec = vec![
-    //     0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, 0x00, 0x00, 0x00, 0x0D, 0x49, 0x48, 0x44,
-    //     0x52, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01, 0x08, 0x06, 0x00, 0x00, 0x00, 0x1F,
-    //     0x15, 0xC4, 0x89, 0x00, 0x00, 0x00, 0x0A, 0x49, 0x44, 0x41, 0x54, 0x78, 0x9C, 0x63, 0x00,
-    //     0x01, 0x00, 0x00, 0x05, 0x00, 0x01, 0x0D, 0x0A, 0x2D, 0xB4, 0x00, 0x00, 0x00, 0x00, 0x49,
-    //     0x45, 0x4E, 0x44, 0xAE, 0x42, 0x60, 0x82,
-    // ];
-
     let mut context = context!(ptr).inner.lock().unwrap();
     context
         .add_copied_input_buffer(io_id, v.as_slice())
         .unwrap();
-    //     .add_copied_input_buffer(io_id, v.as_slice())
-    //     .unwrap();
 
     Ok(atoms::ok().encode(env))
 }
