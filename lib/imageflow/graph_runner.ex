8d50a6469402b8e4e3fd749cad2b040770604ef4
Marcin Adamczyk <adamczyk.marcin@outlook.com>
2021-02-08 16:07:06 +0100

    Add decoding and encoding to string for Graph

--- lib/imageflow/graph_runner.ex
+++ lib/imageflow/graph_runner.ex
@@ -7,8 +7,8 @@ defmodule Imageflow.GraphRunner do
          :ok <- add_outputs(job, graph.outputs),
          :ok <- send_task(job, graph),
          :ok <- save_outputs(job, graph.outputs),
-         :ok <- Native.destroy(job) do
-      :ok
+         {:ok, results} <- print_results(job, graph.outputs) do
+      if results == [], do: :ok, else: {:ok, results}
     end
   end
 
@@ -18,6 +18,7 @@ defmodule Imageflow.GraphRunner do
       {id, value}, :ok ->
         case value do
           {:file, path} -> Native.add_input_file(job, id, path)
+          {:bytes, blob} -> Native.add_input_buffer(job, id, blob)
         end
         |> case do
           :ok -> {:cont, :ok}
@@ -29,10 +30,13 @@ defmodule Imageflow.GraphRunner do
   defp add_outputs(job, inputs) do
     inputs
     |> Enum.reduce_while(:ok, fn
-      {id, _}, :ok ->
-        with :ok <- Native.add_output_buffer(job, id) do
-          {:cont, :ok}
-        else
+      {id, value}, :ok ->
+        case value do
+          {:file, _path} -> Native.add_output_buffer(job, id)
+          :bytes -> Native.add_output_buffer(job, id)
+        end
+        |> case do
+          :ok -> {:cont, :ok}
           {:error, _} = error -> {:halt, error}
         end
     end)
@@ -44,11 +48,27 @@ defmodule Imageflow.GraphRunner do
       {id, value}, :ok ->
         case value do
           {:file, path} -> Native.save_output_to_file(job, id, path)
+          :bytes -> :ok # skip
         end
         |> case do
           :ok -> {:cont, :ok}
           {:error, _} = error -> {:halt, error}
         end
+    end)
+  end
+
+  def print_results(job, outputs) do
+    outputs
+    |> Enum.reduce_while({:ok, []}, fn ({id, value}, {:ok, acc}) ->
+      case value do
+        :bytes -> Native.get_output_buffer(job, id)
+        {:file, _} -> :skip # skip
+      end
+      |> case do
+        :skip -> {:cont, {:ok, acc}}
+        {:ok, results} -> {:cont, {:ok, :binary.list_to_bin(results)}}
+        {:error, _} = error -> {:halt, error}
+      end
     end)
   end
 
